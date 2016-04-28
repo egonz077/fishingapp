2c7a726fa1d2e0f4a942ef45d9b54a9eefd99aa3
Emmanuel Gonzalez <egonz077@fiu.edu>
2016-04-27 23:38:46 -0400

    removed config folder

--- config/config.js
+++
@@ -1,3 +0,0 @@
-var connectionString = process.env.DATABASE_URL || 'postgres://postgres:1234@localhost:5432/fishingapp'
-
-module.exports = connectionString;
--- config/env/development.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  // Development configuration options
-  sessionSecret: 'developmentSessionSecret'
-  wUndergroundkey: '7d2b1a4095dd9a10'
-};
